d8a9bcc2affda50162ad512cf1e463a5416298a7
Diegao <diegonoguerarec@gmail.com>
2026-01-10 16:50:06 -0300

    Final code not working for some reason

--- feedback_form/src/Components/FeedbackForm.jsx
+++ feedback_form/src/Components/FeedbackForm.jsx
@@ -3,15 +3,57 @@ import './FeedbackForm.css'; // Import CSS for styling
 
 const FeedbackForm = () => {
 
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    feedback: ''
+  });
+
+  const handleChange = (event) => {
+    const {name, value} = event.target;
+
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const confirmationMessage = `
+      Name: ${formData.name}
+      Email: ${formData.email}
+      Feedback: ${formData.feedback}
+    `;
+
+    const isConfirmed = window.confirm(`Please confirm your details: \n\n ${confirmationMessage}`);
+  
+    if (isConfirmed) {
+      console.log(`Submitting feedback: ${formData}`);
+      setFormData({
+        name: '',
+        email: '',
+        feedback: ''
+      });
+
+      alert(`Thank you for your valuable feedback!`)
+    }
+  };
+
   return (
     <>
     <nav>
     Tell Us What You Think
     </nav>
-      <form className="feedback-form">
+      <form onSubmit={handleSubmit} className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+
+        <input onChange={handleChange} value={formData.name} type="text" name='name' placeholder='Your Name'/>
+        <input onChange={handleChange} value={formData.email} type="email" name='email' placeholder='Your Email' />
+        <textarea onChange={handleChange} value={formData.feedback} name="feedback" placeholder='Your Feedback'></textarea>
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
   );
